940bf2fa5c71cac53ac94f1e26d56d24ce4bc307
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-04 10:06:10 +0100

    Add insertTable command

---
+++ bower_components/scribe-plugin-inserttable-command/scribe-plugin-inserttable-command.js
@@ -0,0 +1,50 @@
+define('scribe-plugin-inserttable-command',[],function () {
+
+  /**
+   * This plugin adds a command for inserting tables (in Markdown)
+   */
+
+  return function () {
+    return function (scribe) {
+      var insertTableCommand = new scribe.api.Command('insertHTML');
+      insertTableCommand.nodeName = 'PRE';
+
+      insertTableCommand.execute = function () {
+        scribe.api.SimpleCommand.prototype.execute.call(this, '<pre>one|two|three</pre>');
+      };
+
+     insertTableCommand.queryState = function () {
+        /**
+         * We override the native `document.queryCommandState` for links because
+         * the `createLink` and `unlink` commands are not supported.
+         * As per: http://jsbin.com/OCiJUZO/1/edit?js,console,output
+         */
+        var selection = new scribe.api.Selection();
+        return !! selection.getContaining(function (node) {
+          return node.nodeName === this.nodeName;
+        }.bind(this));
+      };
+
+      /**
+       * All: Executing a heading command inside a list element corrupts the markup.
+       * Disabling for now.
+       */
+      insertTableCommand.queryEnabled = function () {
+        var selection = new scribe.api.Selection();
+        var preNode = selection.getContaining(function (node) {
+          return node.nodeName === 'PRE';
+        });
+
+        console.log('INSIDE PRE', preNode);
+
+        return scribe.api.Command.prototype.queryEnabled.apply(this, arguments)
+          && scribe.allowsBlockElements() && ! preNode;
+      };
+
+      scribe.commands.insertTable = insertTableCommand;
+    };
+  };
+
+});
+
+//# sourceMappingURL=scribe-plugin-link-prompt-command.js.map
