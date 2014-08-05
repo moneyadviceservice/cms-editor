9ea124545235f3bc9ef57d3e7b14aa88c36683dd
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-05 13:28:04 +0100

    Fix for sticky toolbar

--- bower_components/scribe-plugin-sticky-toolbar/scribe-plugin-sticky-toolbar.js
+++ bower_components/scribe-plugin-sticky-toolbar/scribe-plugin-sticky-toolbar.js
@@ -1,13 +1,18 @@
 define('scribe-plugin-sticky-toolbar',['scribe-plugin-toolbar'],function (scribePluginToolbar) {
   return function (toolbarNode) {
     return function (scribe) {
-      var toolbarOffsetTop = toolbarNode.offsetTop;
+      var toolbarOffsetTop,
+          scrolled = false;
 
       // Instantiate the toolbar plugin
       scribe.use(scribePluginToolbar(toolbarNode));
 
       // Listen for scroll - TODO Debounce
       document.addEventListener('scroll', function () {
+        if(!scrolled){
+          toolbarOffsetTop = toolbarNode.offsetTop;
+          scrolled = true;
+        }
         toolbarNode.classList.toggle('is-sticky', (toolbarOffsetTop <= window.scrollY));
       });
     };
