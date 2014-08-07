f0bfabc977f217cf77db8561ca332f3b6c01bbd5
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-07 15:31:13 +0100

    Get editor textarea from fixture

--- test/tests/default.spec.js
+++ test/tests/default.spec.js
@@ -1,7 +1,16 @@
 describe('CMS Editor', function() {
   before(function(done) {
-    var fixtureHTML = window.__html__['test/fixtures/default.html'];
-    done();
+    var that = this;
+
+    this.html = window.__html__['test/fixtures/default.html'];
+    document.body.innerHTML = this.html;
+    this.editorSelector = '#editor';
+
+    requirejs(['helpers'],function (helpers) {
+      that.helpers = helpers;
+      that.editor = document.querySelector(that.editorSelector);
+      done();
+    }, done);
   });
 
   describe('Initial test', function() {
