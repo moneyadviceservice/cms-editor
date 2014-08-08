72d0ba7ecd399da6b97c00378533bc3c562d1832
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-08 16:18:42 +0100

    Add sticky toolbar plugin

---
+++ src/modules/plugins/editor-sticky-toolbar.js
@@ -0,0 +1,17 @@
+define('editor-plugin-sticky-toolbar', [], function () {
+  return function(toolbarNode) {
+    return function() {
+      var toolbarOffsetTop,
+          scrolled = false;
+
+      // Listen for scroll - TODO Debounce
+      document.addEventListener('scroll', function () {
+        if(!scrolled){
+          toolbarOffsetTop = toolbarNode.offsetTop;
+          scrolled = true;
+        }
+        toolbarNode.classList.toggle('is-sticky', (toolbarOffsetTop <= window.scrollY));
+      });
+    };
+  };
+});
