9704d7a0f12e8d35823777c909b57b0d55f72df9
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-11 11:14:24 +0100

    Refactor to add setup and teardown

--- test/tests/editor.spec.js
+++ test/tests/editor.spec.js
@@ -1,110 +1,131 @@
 describe('CMS Editor', function() {
   'use strict';
 
-  before(function(done) {
+  beforeEach(function(done) {
     var self = this;
 
-    document.body.innerHTML = window.__html__['test/fixtures/default.html'];
     this.markdownEditorSelector = '#markdown-editor';
     this.htmlEditorSelector = '#html-editor';
+    this.toolbarNodeSelector = '#toolbar';
 
-    requirejs(['helpers','text!test/helpers/content/content.md'], function (helpers, content) {
+    requirejs([
+      'editor',
+      'helpers',
+      'text!test/helpers/content/content.md'
+    ],
+    function (
+      Editor,
+      helpers,
+      content
+    ) {
+      var sandbox = document.createElement('div');
+      sandbox.innerHTML = window.__html__['test/fixtures/default.html'];
+      document.body.appendChild(sandbox);
+      self.sandbox =  sandbox;
       self.helpers = helpers;
-      self.markdownEditor = document.querySelector(self.markdownEditorSelector);
-      self.htmlEditor = document.querySelector(self.htmlEditorSelector);
-
+      self.toolbarNode = sandbox.querySelector(self.toolbarNodeSelector);
+      self.markdownEditor = sandbox.querySelector(self.markdownEditorSelector);
+      self.htmlEditor = sandbox.querySelector(self.htmlEditorSelector);
+      console.log(self.toolbarNode);
+      console.log(self.markdownEditor);
+      console.log(self.htmlEditor);
+      self.Editor = Editor;
       self.markdownEditor.value = content;
       done();
     }, done);
   });
 
-  describe('Mode switch', function() {
-    beforeEach(function(done) {
-      var self = this;
-      requirejs(['editor'],function(Editor) {
-        self.Editor = Editor;
-        done();
-      }, done);
-    });
+  afterEach(function() {
+    this.sandbox.parentNode.removeChild(this.sandbox);
+  });
 
+  describe('Mode switch', function() {
     it('allows the mode to be changed to markdown and content updated', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       expect(false).to.be.true;
     });
 
     it('allows the mode to be changed to html and content updated', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       expect(false).to.be.true;
     });
   });
 
   describe('Markdown editor', function() {
-    beforeEach(function(done) {
-      var self = this;
-      requirejs(['editor'],function(Editor) {
-        self.Editor = Editor;
-        done();
-      }, done);
-    });
-
     it('allows value to be set', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       expect(false).to.be.true;
     });
 
     it('allows value to be read', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       expect(false).to.be.true;
     });
   });
 
   describe('HTML editor', function() {
-    beforeEach(function(done) {
-      var self = this;
-      requirejs(['editor'],function(Editor) {
-        self.Editor = Editor;
-        done();
-      }, done);
-    });
-
     it('allows value to be set', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       expect(false).to.be.true;
     });
 
     it('allows value to be read', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       expect(false).to.be.true;
     });
   });
 
   describe('Plugins', function() {
-    beforeEach(function(done) {
-      var self = this;
-      requirejs(['editor'],function(Editor) {
-        self.Editor = Editor;
-        done();
-      }, done);
-    });
-
     it('allows a plugin to be specified and activated', function() {
-      var editor = new this.Editor(null,null,null,{});
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
       var spy = sinon.spy();
       editor.use(spy);
       expect(spy.callCount).to.equal(1);
     });
 
     it('should pass the plugin an editor instance as an argument', function() {
-      var editor = new this.Editor(null,null,null,{});
-      var pluginSpy;
+      var editor = new this.Editor(
+        this.htmlEditor,
+        this.markdownEditor,
+        this.toolbarNode,
+        {});
+      var toolbarInstance;
       var plugin = function(){
         return function(instance) {
-          pluginSpy = instance;
+          toolbarInstance = instance;
         };
       };
       var spy = sinon.spy(editor, 'use');
       editor.use(plugin());
-      expect(pluginSpy).to.equal(editor);
+      expect(toolbarInstance).to.equal(editor);
     });
 
   });
