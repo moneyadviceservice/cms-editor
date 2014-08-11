2ee008dc8e56dc3bcb101feb9c129c4a895e28ee
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-11 11:19:37 +0100

    Add some jsdocs documentation

--- src/editor.js
+++ src/editor.js
@@ -7,6 +7,14 @@ define('editor', [
 ) {
     'use strict';
 
+    /**
+     * Creates an editor instance
+     * @class
+     * @param {Object} htmlEditorNode       DOM node to attach the HTML editor
+     * @param {Object} markdownEditorNode   DOM node to attach the Markdown editor
+     * @param {Object} toolbarNode          DOM node for the HTML editor toolbar
+     * @param {Object} options              Options
+     */
     var Editor = function(htmlEditorNode, markdownEditorNode, toolbarNode, options) {
       this.toolbarNode = toolbarNode;
       this.htmlEditorNode = htmlEditorNode;
@@ -20,73 +28,128 @@ define('editor', [
       this._init();
     };
 
+    /**
+     * Starts up the class
+     * @return {Object} this
+     */
     Editor.prototype._init = function() {
       this.changeEditingMode(this.mode);
       return this;
     };
 
+    /**
+     * Converts HTML to Markdown
+     * @private
+     * @param {string} html    HTML to be converted to Markdown
+     * @return {string}
+     */
     Editor.prototype._convertHTMLToMarkdown = function(html) {
-      this.converter.toMarkdown(html);
-      return this;
+      return this.converter.toMarkdown(html);
     };
 
+    /**
+     * Converts Markdown to HTML
+     * @private
+     * @param {string} markdown     Markdown to be converted to HTML
+     * @return {string}
+     */
     Editor.prototype._convertMarkdownToHTML = function(markdown) {
-      this.converter.toHTML(markdown);
-      return this;
+      return this.converter.toHTML(markdown);
     };
 
-    Editor.prototype.setEditingMode = function(mode) {
+    /**
+     * Sets mode property
+     * @private
+     * @param {string} mode  HTML to be converted to Markdown
+     * @return {string}
+     */
+    Editor.prototype._setEditingMode = function(mode) {
       this.options.mode = mode;
       return this;
     };
 
+    /**
+     * Changes the editing mode
+     * @param  {string} mode html|markdown
+     * @return {Object}      this
+     */
     Editor.prototype.changeEditingMode = function(mode) {
       var dispatch = {
         'markdown': function() {
           console.log('markdown');
+          // this.setHTMLContent();
         },
         'html': function() {
           console.log('html');
         }
       },
       dispatchElse = function() {
-        throw new Error('That conversion isn\'t supported yet');
+        throw new Error('That conversion isn\'t supported');
       };
 
       (dispatch[mode] || dispatchElse())();
-      // this.setEditingMode(mode);
+      this._setEditingMode(mode);
       // this.setContent();
       return this;
     };
 
+    /**
+     * Sets the initial content value depending on the mode
+     * @param {string}[html|markdown] src The parsed source code
+     * @param {string} mode  The mode
+     */
     Editor.prototype.setContent = function(src, mode) {
       // this.modes[this.mode].setContent(src);
-      // Should convert src to mode
       return this;
     };
 
+    /**
+     * Gets the contents of the HTML editor
+     * @return {string} HTML
+     */
     Editor.prototype.getHTMLContent = function() {
       return this;
     };
 
+    /**
+     * Sets the HTML editor's content
+     * @param {string} html    HTML content
+     * @return {Object} this
+     */
     Editor.prototype.setHTMLContent = function(html) {
-      console.log(markdown);
       return this;
     };
 
+    /**
+     * Gets the content of the Markdown `textarea` box
+     * @return {string} Markdown
+     */
     Editor.prototype.getMarkdownContent = function() {
       return this;
     };
 
+    /**
+     * Sets the Markdown editor's content
+     * @param {string} markdown    Markdown content
+     * @return {Object} this
+     */
     Editor.prototype.setMarkdownContent = function(markdown) {
-      console.log(markdown);
       return this;
     };
 
+    /**
+     * Boots up the display of the content
+     * @return {Object} this
+     */
     Editor.prototype.render = function() {
       return this;
     };
 
+    /**
+     * Starts a plugin
+     * @param  {Object} activatePlugin The plugin to start
+     * @return {Object} this
+     */
     Editor.prototype.use = function(activatePlugin) {
       activatePlugin(this);
       return this;
