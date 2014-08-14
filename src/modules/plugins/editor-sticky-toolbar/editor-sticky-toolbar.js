500229faae2ca142fbf8f80a8f97d13651682fd3
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-14 14:53:48 +0100

    Check for offsetTop from body element as opposed to from parent

--- src/modules/plugins/editor-sticky-toolbar/editor-sticky-toolbar.js
+++ src/modules/plugins/editor-sticky-toolbar/editor-sticky-toolbar.js
@@ -4,10 +4,20 @@ define('editor-plugin-sticky-toolbar', [], function () {
       var toolbarOffsetTop,
           scrolled = false;
 
+      var getViewportOffsetTop = function(node) {
+        var curTop = 0;
+
+        while (node.tagName !== 'BODY') {
+          curTop += node.offsetTop;
+          node = node.offsetParent;
+        }
+        return curTop;
+      };
+
       // Listen for scroll - TODO Debounce
       document.addEventListener('scroll', function () {
         if(!scrolled){
-          toolbarOffsetTop = toolbarNode.offsetTop;
+          toolbarOffsetTop = getViewportOffsetTop(toolbarNode);
           scrolled = true;
         }
         toolbarNode.classList.toggle('is-sticky', (toolbarOffsetTop <= window.scrollY));
