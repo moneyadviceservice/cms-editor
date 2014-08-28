5e316a15f13b7ce6c4cf307977b2a3d857df8556
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-28 09:41:24 +0100

    Tidy conditional

--- src/modules/plugins/editor-auto-resize-textarea/editor-auto-resize-textarea.js
+++ src/modules/plugins/editor-auto-resize-textarea/editor-auto-resize-textarea.js
@@ -3,8 +3,7 @@ define('editor-plugin-auto-resize-textarea', [], function () {
   return function(markdownEditorNode, options) {
     return function(editor) {
       editor.events.subscribe('content:updated', function(mode) {
-        if(mode === editor.constants.MODES.MARKDOWN &&
-          markdownEditorNode.scrollHeight > (options && options.minHeight? options.minHeight : 200)) {
+        if(mode === editor.constants.MODES.MARKDOWN && markdownEditorNode.scrollHeight > (options && options.minHeight? options.minHeight : 200)) {
           markdownEditorNode.style.height = 0;
           markdownEditorNode.style.height = markdownEditorNode.scrollHeight + 'px';
         }
