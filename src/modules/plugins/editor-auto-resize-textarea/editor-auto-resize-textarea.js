124aa4bf1d87ef928f0092d8ad597d15c4ea6cb8
Justin Perry <justin.perry@moneyadviceservice.org.uk>
2014-08-28 10:31:03 +0100

    Add UI input event listener and a temp fix for the scrolling issue

--- src/modules/plugins/editor-auto-resize-textarea/editor-auto-resize-textarea.js
+++ src/modules/plugins/editor-auto-resize-textarea/editor-auto-resize-textarea.js
@@ -2,10 +2,17 @@ define('editor-plugin-auto-resize-textarea', [], function () {
   'use strict';
   return function(markdownEditorNode, options) {
     return function(editor) {
-      editor.events.subscribe('content:updated', function(mode) {
-        if(mode === editor.constants.MODES.MARKDOWN && markdownEditorNode.scrollHeight > (options && options.minHeight? options.minHeight : 200)) {
+      var handleContentUpdate = function () {
+        if(markdownEditorNode.scrollHeight > (options && options.minHeight? options.minHeight : 200)) {
           markdownEditorNode.style.height = 0;
           markdownEditorNode.style.height = markdownEditorNode.scrollHeight + 'px';
+          markdownEditorNode.scrollTop = markdownEditorNode.scrollHeight;
+        }
+      };
+      markdownEditorNode.addEventListener('input', handleContentUpdate);
+      editor.events.subscribe('content:updated', function(mode) {
+        if(mode === editor.constants.MODES.MARKDOWN) {
+          handleContentUpdate(mode);
         }
       });
     };
